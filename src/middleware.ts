7d39e399a6505182fc6a12bf4a13e1b6cf0ea765
Satheesh B <ananthisatheesh123@gmail.com>
2026-08-27 17:41:10 +0530

    temp: debug middleware basePath/pathname

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,17 @@
+import { NextResponse } from "next/server";
 import NextAuth from "next-auth";
 import { authConfig } from "@/auth.config";
 
-export default NextAuth(authConfig).auth;
+const handler = NextAuth(authConfig).auth;
+
+export default async function middleware(req: import("next/server").NextRequest) {
+  const res = await handler(req as any, { params: {} } as any);
+  if (res) {
+    res.headers.set("x-debug-base", req.nextUrl.basePath || "(none)");
+    res.headers.set("x-debug-path", req.nextUrl.pathname);
+  }
+  return res;
+}
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|external|.*\\.png$).*)"],
