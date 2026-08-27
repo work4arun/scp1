89430694b4577a35fae4183e5fd5dce8723323c7
Satheesh B <ananthisatheesh123@gmail.com>
2026-08-27 17:48:47 +0530

    Restore clean middleware (basePath fix lives in auth.config)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,7 @@
-import { NextResponse } from "next/server";
 import NextAuth from "next-auth";
 import { authConfig } from "@/auth.config";
 
-const handler = NextAuth(authConfig).auth;
-
-export default async function middleware(req: import("next/server").NextRequest) {
-  const res = await handler(req as any, { params: {} } as any);
-  if (res && res instanceof NextResponse) {
-    res.headers.set("x-debug-base", req.nextUrl.basePath || "(none)");
-    res.headers.set("x-debug-path", req.nextUrl.pathname);
-    res.headers.set("x-debug-handler", "true");
-  } else {
-    const r = NextResponse.next();
-    r.headers.set("x-debug-base", req.nextUrl.basePath || "(none)");
-    r.headers.set("x-debug-path", req.nextUrl.pathname);
-    r.headers.set("x-debug-handler", "false");
-    return r;
-  }
-  return res;
-}
+export default NextAuth(authConfig).auth;
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|external|.*\\.png$).*)"],
